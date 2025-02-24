e635870a915c4989c0f55842159b2c010ca49473
JeanYoungPark <jean.young.park.96@gmail.com>
2025-02-24 13:45:57 +0900

    base url 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/css/common.min.css";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter basename='/js/game/supplement/starwords'>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
 
